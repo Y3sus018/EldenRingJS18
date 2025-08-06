5a10707002081b39ed6fe42e28c4bde6d69dd346
angeldavid2004 <56angelaguilar@gmail.com>
2025-08-06 17:00:55 -0600

    Angel

---
+++ regitro.php
@@ -0,0 +1,9 @@
+<?php
+//incluiremos el archivos externos
+include'Conexión.php';
+//vamos acapturar los datos del formulario
+$nombre=$_nombrePOST['nobre'];
+$email=$_nombrePOST['email'];
+$Contraseña=$_nombrePOST['Contraseña'];
+
+?>
